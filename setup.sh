10637a28eb5d57acf45bab5f90bd1b77a6844432
esdrasgc <esdrasgcdev@gmail.com>
2024-10-24 19:26:45 -0300

    copia do .env e source na venv

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 ./setup_env.sh
 
+cp .env ./vector_db/
+
 ./setup_python.sh
 
 docker network create chat-network
 
 cd vector_db/
 
-docker compose down
+docker-compose down
 
 docker-compose up -d 
 
 cd ..
+
+source /venv/bin/activate
 
 python ./data_science/RAG_Engineering/main_faster.py
 
